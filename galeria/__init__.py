30fcfb70eb8b84f79a0ca05b60494f7f5726d3e1
klaus AG <kallendgehrke@gmail.com>
2024-07-04 17:08:50 -0300

    correção banco de dados

--- galeria/__init__.py
+++ galeria/__init__.py
@@ -6,7 +6,6 @@ import os
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///comunidade.db"
 app.config["SECRET_KEY"] = "8814a23498a7ef3179f61afeb8cb87c6"
 app.config["UPLOAD_FOLDER"] = "static/fotos_posts"
 

07a01d9828c360bf64ed2839aecd45bc922f2eff
klaus AG <kallendgehrke@gmail.com>
2024-07-04 16:55:02 -0300

    mudança banco postgresql

--- galeria/__init__.py
+++ galeria/__init__.py
@@ -5,6 +5,7 @@ from flask_bcrypt import Bcrypt
 import os
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE.URL")
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///comunidade.db"
 app.config["SECRET_KEY"] = "8814a23498a7ef3179f61afeb8cb87c6"
 app.config["UPLOAD_FOLDER"] = "static/fotos_posts"
